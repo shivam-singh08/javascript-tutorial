b2c90cabb15ffb14c640e20fc8308e8ad4bafd22
Shivam <shivahumai08@gmail.com>
2024-08-04 13:32:40 +0530

    comparsion

---
+++ basic/05_comparsion.js
@@ -0,0 +1,19 @@
+/**
+ * comparsions
+ */
+
+console.log('2'>1); //true
+console.log('02' > 1); //true
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true. 
+
+console.log(undefined == 0); //false
+console.log(undefined == null); //true
+
+/**
+ * === --> strict type check check for value and its type.
+ * == --> check for the value not its types.
+ *
+ */
