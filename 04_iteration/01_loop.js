764831874ededca293ca906ccd3114b95919fa64
Shivam <shivahumai08@gmail.com>
2024-08-06 21:10:14 +0530

    loops

---
+++ 04_iteration/01_loop.js
@@ -0,0 +1,35 @@
+/**
+ * for loop
+ */
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(i);
+// }
+
+/**
+ * break and continue
+ */
+
+// for (let index = 0; index < 20; index++) {
+//     if (index == 5) {
+//         console.log('detected 5');
+//             break;   
+//     }
+//     console.log(`value of i is ${index}`);
+        
+// }
+
+//while loop do while loop
+// let index = 0;
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index+=2;
+// }
+
+//do while
+let i = 0
+do {
+    console.log(i);
+    i++  
+    
+} while (i <= 1);
