a4a64504d8ab7e853442efe5f443ee9623659c9d
Shivam <shivahumai08@gmail.com>
2024-08-08 16:35:57 +0530

    forEach

---
+++ 04_iteration/03_forEach.js
@@ -0,0 +1,52 @@
+/**
+ * for each -->
+ * 
+ */
+
+const coding  = ['js', 'ruby', 'java', 'py', 'cpp'];
+
+// coding.forEach(function (item){
+//     console.log(item); 
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item); 
+// }
+
+// coding.forEach(printMe) //pass a function as parameter pass as reference don't call function ().
+
+//for each have the each value of array n its index also a complete array.
+
+coding.forEach((item,index,arr)=>{
+    //console.log(item, index, arr);
+    /**
+     *  js 0 [ 'js', 'ruby', 'java', 'py', 'cpp' ]
+        ruby 1 [ 'js', 'ruby', 'java', 'py', 'cpp' ]
+        java 2 [ 'js', 'ruby', 'java', 'py', 'cpp' ]
+        py 3 [ 'js', 'ruby', 'java', 'py', 'cpp' ]
+        cpp 4 [ 'js', 'ruby', 'java', 'py', 'cpp' ]
+     */
+    
+})
+
+const myCoding =[{
+    langName: 'javascript',
+    langFileName:'js'
+},
+{
+    langName: 'Python',
+    langFileName: 'py',
+},{
+    langName: 'java',
+    langFileName: 'java'
+}]
+
+myCoding.forEach((item)=>{
+    console.log(item.langName);
+    
+})
